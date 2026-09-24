40856e1d6d47fc7a73f681f06c20021d69374ad4
Haji Suleman <mirhajisulemanjamali@gmail.com>
2026-09-24 12:27:41 +0500

    adding about us page

--- client/src/pages/CommentData.jsx
+++ client/src/pages/CommentData.jsx
@@ -25,20 +25,8 @@ const CommentData = () => {
     };
     return (
         <>
-            <div className="reviews-top-link-wrapper">
-                <a href="/shop" className="reviews-top-link">
-                    Shop All Patterns
-                </a>
-            </div>
 
-            {/* Header Section */}
-            <div className="reviews-header">
-                <h1>Our happy customers are saying</h1>
-                <div className="reviews-rating-summary">
-                    <span className="reviews-stars">★★★★★</span>
-                    <span className="reviews-score">4.86 ★ (1,593)</span>
-                </div>
-            </div>
+        
             <div className="comments-wrapper">
                 <div className="comments-list-wrapper">
                     <button
