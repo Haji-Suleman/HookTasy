a597f7f8222071a22199134da19cc1775bcfb467
Haji Suleman <mirhajisulemanjamali@gmail.com>
2026-09-27 00:06:57 +0500

    adding newthing

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,7 @@ export const connectDB = async () => {
   try {
     console.log("🚀 Attempting to connect to MongoDB...");
     const conn = await mongoose.connect(
-      "mongodb+srv://zootsyshops_db_user:E36tn2x43DShyt1U@zootsy.hxrznyi.mongodb.net/"
+      process.env.MONGODB_URL
     );
     console.log(`✅ MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
